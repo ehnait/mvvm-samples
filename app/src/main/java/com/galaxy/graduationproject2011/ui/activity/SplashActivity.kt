b72003682806f8c02a5a082fb46cbd0ad96c6325
Liam <17611000331@163.com>
2021-03-16 15:48:08 +0800

    add  codes

--- app/src/main/java/com/galaxy/graduationproject2011/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/galaxy/graduationproject2011/ui/activity/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.galaxy.graduationproject2011.ui.activity
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.galaxy.common.base.BaseActivity
 import com.galaxy.common.extension.showShortToast
 import com.galaxy.common.extension.start
 import com.galaxy.common.utils.PreferenceUtils
 import com.galaxy.graduationproject2011.R
 import com.galaxy.graduationproject2011.entity.Constant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Created by Liam.Zheng on 2020/10/16
@@ -22,15 +27,19 @@ class SplashActivity : BaseActivity() {
 
     override fun initView(savedInstanceState: Bundle?) {
         if (firstStart) {
-            showShortToast("firstStart-firstStart-firstStart")
-        } else {
+            showShortToast("FIRST START")
+        }
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                delay(1500)
+                if (spUserName.isEmpty()) {
+                    start<LoginActivity>()
+                } else {
+                    start<MainActivity>()
+                }
+            }
+        }
 
-        }
-        if (spUserName.isEmpty()) {
-            start<LoginActivity>()
-        } else {
-            start<MainActivity>()
-        }
     }
 
     override fun initData() {
