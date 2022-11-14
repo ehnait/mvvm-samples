88269bb7c1dcbcee92b935f1715242c670a73a52
ehnait <17611000331@163.com>
2022-11-14 16:43:01 +0800

    update

--- module-base/lib-common/src/main/java/com/example/lib_common/ext/Flow.kt
+++ module-base/lib-common/src/main/java/com/example/lib_common/ext/Flow.kt
@@ -1,30 +1,20 @@
 package com.example.lib_common.ext
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.example.lib_common.R
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.flowWithLifecycle
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
-import java.net.SocketException
-import java.net.SocketTimeoutException
 
-inline fun <T:Any> Flow<T>.launchAndCollectIn(
-    owner: LifecycleOwner,
+fun <T> Flow<T>.collectWithLifecycle(
+    lifecycle: Lifecycle,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-    crossinline action: suspend CoroutineScope.(data:T) -> Unit
-) = owner.lifecycleScope.launch {
-    owner.repeatOnLifecycle(minActiveState) {
-        collect {
-            action(it)
-        }
+    collector: FlowCollector<T>
+) {
+    lifecycle.coroutineScope.launch {
+        flowWithLifecycle(lifecycle, minActiveState).collect(collector)
     }
 }
 
